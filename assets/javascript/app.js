5e17134ecf6e09d0bb7291266ae2110e3c81f240
jbhafner <brian_hafner@yahoo.com>
2017-08-30 08:46:38 -0700

    question looping and onclick functions working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+var game = {
+    count: 30,
+    win: 0,
+    loss: 0,
+    counter: 5
+}
+
 
 // Questions Object contains all the questions and related info //
 var questions = [{
@@ -47,13 +54,90 @@ var questions = [{
     image: "assets/images/NZFlag.jpg"       
 }];
 
-// Functions Section //
-$(document).ready(function(){
-    $(function(){
-        // $("#answer1").hover(highlight, highlight);
-        $("#answer1").on("mouseover mouseleave", highlight, highlight);
-    });
-    console.log("ready!");
+// *** Functions Section *** //
+
+console.log(questions[1].question);
+console.log("questions array length is " + questions.length);    
+
+$("#answer1").on("mouseover mouseleave", highlight);
+$("#answer2").on("mouseover mouseleave", highlight);
+$("#answer3").on("mouseover mouseleave", highlight);
+$("#answer4").on("mouseover mouseleave", highlight);
+
+$( "#answer1" ).on( "click", function() {
+  debugClick(1);
 });
+$( "#answer2" ).on( "click", function() {
+  debugClick(2);
+});
+$( "#answer3" ).on( "click", function() {
+  debugClick(3);
+});
+$( "#answer4" ).on( "click", function() {
+  debugClick(4);
+});        
+        // $("#answer1").click(debugClick(1));
+        //$("#answer2").on("click", debugClick(2));
+        // $("#answer3").on("click", debugClick);
+        // $("#answer4").on("click", debugClick);
+
+
+
+// $( "#answer2" ).on( "click", function() {
+//   debugClick(33);
+// });
+
+
+
+
+
+
+function highlight(evt) {
+    $(this).toggleClass("highlighted");        
+};
+
+function debugClick(num) {
+    console.log("Button Click " + num);
+};
+
+//initialize play loop
+var length = questions.length;
+var i = 0;
+var maxTime = 5;
+console.log("test - length is " + length);
+
+// Update HTML Functions //
+function updateQuestion() {
+    $("#question").html(questions[i].question);
+    $("#answer1").html(questions[i].choices[0]);
+    $("#answer2").html(questions[i].choices[1]);
+    $("#answer3").html(questions[i].choices[2]);
+    $("#answer4").html(questions[i].choices[3]);        
+};
+
+updateQuestion();   
+
+console.log ("i is " + i);
+console.log(questions[i].question);
+
+var timer=setInterval(function(){
+
+    if (maxTime===0) {  
+    // only run after 30 secs
+        i++;
+        maxTime=5;
+    } // where does this go???
+
+    updateQuestion();
+    maxTime--;
+
+
+    $("#timeRemaining").html("Time remaining: " + maxTime);
+
+}, 1000);
+
+// *** Main Program Logic *** //
+
+// updateQuestion();
 
 
