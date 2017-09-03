40a5d2206f8facbe040cd3573801d76c66795d06
jbhafner <brian_hafner@yahoo.com>
2017-09-02 23:21:06 -0700

    fixed scoping problem on js line 214

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,6 @@
 // game object to store game information // 
-var game = {
+const GAME = {
     currentIndex: 0,
-    numberQuestions: 0,
-    playTime: 5,
-    delayTime: 5,
     answerCorrect: 0,
     answerWrong: 0,
     answerNone: 0,
@@ -11,9 +8,13 @@ var game = {
     correctAnswer: ""
 }
 
+const GAMETIMING = {
+    PLAYTIME: 15,
+    DELAYTIME: 10
+};
+
 // declare my timer function
 var timer;
-var i=game.currentIndex;
 
 // questions array of objects contains all the questions and related info //
 var questions = [{
@@ -70,10 +71,7 @@ console.log("questions array length is " + questions.length);
 
 // Mouse hover over events //
 
-$("#answer1").on("mouseover mouseleave", highlight);
-$("#answer2").on("mouseover mouseleave", highlight);
-$("#answer3").on("mouseover mouseleave", highlight);
-$("#answer4").on("mouseover mouseleave", highlight);
+$(".answer").on("mouseover mouseleave", highlight);
 
 // Highlight function //
 
@@ -83,37 +81,13 @@ function highlight(evt) {
 
 // Answer on click events //
 
-$( "#answer1" ).on( "click", function() {
+$( ".answer" ).on( "click", function() {
+  let id = $(this).attr('id'); // e.g. "answer1"
+  id = parseInt(id.replace("answer",''));
   clearInterval(timer);
-  debugClick(1);
-  if (game.correctIndex===0){
-    rightAnswer();
-  } else {
-    wrongAnswer();
-  }
-});
-$( "#answer2" ).on( "click", function() {  
-  clearInterval(timer);  
-  debugClick(2);
-    if (game.correctIndex===1){
-    rightAnswer();
-  } else {
-    wrongAnswer();
-  }
-});
-$( "#answer3" ).on( "click", function() { 
-  clearInterval(timer);      
-  debugClick(3);
-    if (game.correctIndex===2){
-    rightAnswer();
-  } else {
-    wrongAnswer();
-  }
-});
-$( "#answer4" ).on( "click", function() {
-  clearInterval(timer);      
-  debugClick(4);
-    if (game.correctIndex===3){
+  $("#timeRemaining").hide();
+  debugClick(id);
+  if (GAME.correctIndex===id-1){
     rightAnswer();
   } else {
     wrongAnswer();
@@ -124,18 +98,15 @@ $( "#answer4" ).on( "click", function() {
 
 function debugClick(num) {
     console.log("Button Click " + num + " // correctIndex is index " 
-        + game.correctIndex + " // correctAnswer is index " + game.correctAnswer + " // current Index is index " + game.currentIndex);
+        + GAME.correctIndex + " // correctAnswer is index " + GAME.correctAnswer + " // current Index is index " + GAME.currentIndex);
 };
 
 // startButton Function for first screen //
 
 function startButton () {
-    $("#timeRemaining").html(" ");               
+    $("#timeRemaining").hide();               
     $("#question").html("Start!");
-    $("#answer1").html(" ");
-    $("#answer2").html(" ");
-    $("#answer3").html(" ");
-    $("#answer4").html(" ");
+    $(".answer").html(" ");
     $( "#question" ).on( "click", function() {
         updateQuestion();  // prints first answer to the screen
         gameTimer(); // starts timer running
@@ -145,25 +116,26 @@ function startButton () {
 // updateQuestion updates the screen with the current question //
 
 function updateQuestion() {
-    i=game.currentIndex;
+    let i=GAME.currentIndex;
 
+    $("#timeRemaining").show().html(" ");
     $("#question").html(questions[i].question);
     $("#answer1").html(questions[i].choices[0]);
     $("#answer2").html(questions[i].choices[1]);
     $("#answer3").html(questions[i].choices[2]);
     $("#answer4").html(questions[i].choices[3]);
-    game.correctIndex=questions[i].correctAnswer;
-    game.correctAnswer=questions[i].choices[game.correctIndex];
-    console.log ("var i = " + i + " // currentIndex = " + game.currentIndex + " // quest index is " + questions[i].correctAnswer + 
-    " // question is " + questions[i].choices[game.correctIndex]);
+    GAME.correctIndex=questions[i].correctAnswer;
+    GAME.correctAnswer=questions[i].choices[GAME.correctIndex];
+    console.log ("var i = " + i + " // currentIndex = " + GAME.currentIndex + " // quest index is " + questions[i].correctAnswer + 
+    " // question is " + questions[i].choices[GAME.correctIndex]);
 };
 
 // These three function handle rightAnswer, wrongAnswer, and noAnswer
 
 function rightAnswer() {
-    game.answerCorrect++;
+    GAME.answerCorrect++;
     $("#answer1").html(" ");
-    $("#answer2").html("You are right! " + game.correctAnswer + " is the correct answer.");
+    $("#answer2").html("You are right! " + GAME.correctAnswer + " is the correct answer.");
     $("#answer3").html(" ");
     $("#answer4").html(" ");   
     // add flickr api image
@@ -171,20 +143,20 @@ function rightAnswer() {
 }
 
 function wrongAnswer() {
-    game.answerWrong++;
+    GAME.answerWrong++;
     $("#answer1").html(" ");
     $("#answer2").html("Sorry, you are wrong.")
-    $("#answer3").html("The correct answer is " + game.correctAnswer + ".");
+    $("#answer3").html("The correct answer is " + GAME.correctAnswer + ".");
     $("#answer4").html(" "); 
     //add flickr api image
     delay();
 }
 
 function noAnswer(){
-    game.answerNone++;
-    // console.log("game.AnswerNone is " + game.answerNone);
+    GAME.answerNone++;
+    // console.log("GAME.AnswerNone is " + GAME.answerNone);
     $("#question").html("Out of Time!");
-    $("#answer1").html("The correct answer is " + game.correctAnswer);
+    $("#answer1").html("The correct answer is " + GAME.correctAnswer);
     $("#answer2").html(" ");
     $("#answer3").html(" ");
     $("#answer4").html(" ");               
@@ -195,118 +167,69 @@ function noAnswer(){
 // This function is the delay after displaying the results of the answer selection //
 
 function delay() {
-    console.log("current index is " + game.currentIndex + " // questions index length is " + questions.length);
-    if (game.currentIndex+1===questions.length) {
+    console.log("current index is " + GAME.currentIndex + " // questions index length is " + questions.length);
+    if (GAME.currentIndex+1===questions.length) {
         gameOver();
     };
-    var dispTime=game.delayTime;
-    game.currentIndex++; 
+    var dispTime=GAMETIMING.DELAYTIME;
+    GAME.currentIndex++; 
     setTimeout(function(){
     // console.log ("dispTime display delay is " + dispTime);
         updateQuestion();
         gameTimer();       
-    }, 1000 * game.delayTime);
+    }, 1000 * GAMETIMING.DELAYTIME);
 };
 
 // gameOver and resetGame are meant to reset the game once the game is over //
 
 function gameOver() {
     $("#question").html("Game Over!");
-    $("#answer1").html("Correct Answers: " + game.answerCorrect);
-    $("#answer2").html("Wrong Answers: " + game.answerWrong);
-    $("#answer3").html("No response: " + game.answerNone);
+    $("#answer1").html("Correct Answers: " + GAME.answerCorrect);
+    $("#answer2").html("Wrong Answers: " + GAME.answerWrong);
+    $("#answer3").html("No response: " + GAME.answerNone);
     $("#answer4").html(" ");
     setTimeout(function(){
         resetGame();       
         startButton();
-    }, 1000 * game.delayTime);     
+    }, 1000 * GAMETIMING.DELAYTIME);     
 };
 
+// Reset all Game variables
 function resetGame() {
-    game.currentIndex= 0;
-    game.numberQuestions= 0;
-    game.playTime = 5;
-    game.delayTime= 5;
-    game.answerCorrect= 0;
-    game.answerWrong= 0;
-    game.answerNone= 0;
-    game.correctIndex= 0;
-    game.correctAnswer = "";    
+    GAME.currentIndex= 0;
+    GAME.answerCorrect= 0;
+    GAME.answerWrong= 0;
+    GAME.answerNone= 0;
+    GAME.correctIndex= 0;
+    GAME.correctAnswer = "";    
 }
 
-console.log ("i is " + game.currentIndex + " // question is " + questions[game.currentIndex].question);
+console.log ("i is " + GAME.currentIndex + " // question is " + questions[GAME.currentIndex].question);
 
 // gameTimer function is meant to loop trough the display of the questions //
 
 function gameTimer() {
-    var maxTime = game.playTime;
+    var maxTime = GAMETIMING.PLAYTIME;
 
-    var timer=setInterval(function(){
+    timer=setInterval(function(){
         console.log ("maxTime is " + maxTime + " // timer before loop is " + timer);
         if (maxTime===0) {
             console.log("I'm in");
             clearInterval(timer);
             noAnswer();  
         }
-        
-        // *** Note: i tried putting the on click functions inside the timer setInterval loop so I could clear the timer,
-        //     but then it kept running the click function multiple times
-
-        // $( "#answer1" ).on( "click", function() {
-        //     debugClick(1);
-        //     clearInterval(timer);
-        //     if (game.correctIndex===0){
-        //         rightAnswer();
-        //     } else {
-        //         wrongAnswer();
-        //     }
-        // });
-
-        // $( "#answer2" ).on( "click", function() {
-        //     clearInterval(timer);
-        //     debugClick(2);
-        //     if (game.correctIndex===1){
-        //         rightAnswer();
-        //     } else {
-        //         wrongAnswer();
-        //     }
-        // });
-
-        // $( "#answer3" ).on( "click", function() {
-        //     debugClick(3);
-        //     clearInterval(timer);
-        //     if (game.correctIndex===2){
-        //         rightAnswer();
-        //     } else {
-        //         wrongAnswer();
-        //     }
-        // });
-
-        // $( "#answer4" ).on( "click", function() {
-        //   clearInterval(timer);
-        //   debugClick(4);
-        //     if (game.correctIndex===3){
-        //     rightAnswer();
-        //   } else {
-        //     wrongAnswer();
-        //   }
-        // }); 
-        
+                
         // Displays the time remaining on the screen
-        $("#timeRemaining").html("Time remaining: " + maxTime);
+        $("#timeRemaining").show().html("Time remaining: " + maxTime);
         maxTime--;
     }, 1000);
     // return timer;
-    console.log ("Current Index = " + game.currentIndex + 
-    " // No Answer = " + game.answerNone + " // Correct Answer = " 
-    + game.answerCorrect + " // Wrong Answer = " + game.answerWrong);
+    console.log ("Current Index = " + GAME.currentIndex + 
+    " // No Answer = " + GAME.answerNone + " // Correct Answer = " 
+    + GAME.answerCorrect + " // Wrong Answer = " + GAME.answerWrong);
 };
 
 // ** Program Logic ** //
 
 startButton(); // brings up start screen and starts program running
 
-// I think I need to have the timer function only run once when it is called.  Then check no answer, correct answer or false answer.
-// I just realize the reazon that I get teh error at the end is that it keeps looping.  It does not stop at game.length
-// how do I convert var timer = function() into function timer()?
-// how do I handle the on.click events with regards to the timer function?
